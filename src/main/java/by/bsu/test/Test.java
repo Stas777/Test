c5627d43719609c3374fd46324b4f8c8269e4116
Stas777 <stas@MacBook-Air-Stas.local>
2015-10-27 00:31:11 +0300

    Created Test.java class with "Hello World!" text output, executable Test.jar file
    Created Test.java class with "Hello World!" output text, executable
    Test.jar file

---
+++ src/main/java/by/bsu/test/Test.java
@@ -0,0 +1,12 @@
+package by.bsu.test;
+
+/**
+ * Created by stas on 23.10.15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
